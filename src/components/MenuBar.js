48145627c8eed585b97151beb7cd951f0b1e7cce
magic-onion <alexander.m.truong@gmail.com>
2018-12-13 10:01:23 -0500

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 const MenuBar = (props) => {
-
   /*
 
-  The 'a' tags below are the menu items. Think about the way a menu 
+  The 'a' tags below are the menu items. Think about the way a menu
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
@@ -15,19 +14,19 @@ const MenuBar = (props) => {
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a onClick={props.clickEvent} className="item active" id="profile">
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a onClick={props.clickEvent} className="item" id="photo">
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a onClick={props.clickEvent} className="item" id="cocktail">
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a onClick={props.clickEvent} className="item" id="pokemon">
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -4,6 +4,33 @@ import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
 
+  state = {
+    div: ''
+  }
+
+  handleClicks = (event) => {
+    const bullShit = Array.from(event.target.parentElement.children)
+    this.setState({
+      div: this.whichComp(event.target.id)})
+    bullShit.forEach(bull => bull.className = 'item')
+    event.target.className = 'item active'
+  }
+
+  whichComp = (id) => {
+    switch (id) {
+      case 'cocktail':
+        return <Cocktails />
+      case 'pokemon':
+        return <Pokemon />
+      case 'profile':
+        return <Profile />
+      case 'photo':
+        return <Photos />
+    }
+  }
+
+
+
 
   render() {
 
@@ -13,16 +40,17 @@ class MainBox extends React.Component {
 
     */
 
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
+    // const detailsToDisplay = <div><Cocktail /></div>
 
     return (
       <div>
-        <MenuBar />
-        {detailsToDisplay}
+        <MenuBar clickEvent={this.handleClicks} itemActive={"item active"}/>
+        {this.state.div}
       </div>
     )
   }
 
 }
 
+const styling = {display:'none'}
 export default MainBox
